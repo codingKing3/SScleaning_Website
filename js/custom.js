3acf805c9c864d82008e8d0c71b3e0088d68acce
King J <king3john@gmail.com>
2020-05-15 23:33:52 -0400

    major style change

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,24 @@
 $(function(){
 
-$("#turnOff").hide(); //slow , fast
-$("li").eq(1).hide();
-$("li").eq(2).hide();
-$("#p_number").show('slow');
+//$("#turnOff").hide(); //slow , fast
+$("a").eq(1).hide();
+$("a").eq(2).hide();
 $("h2").css("color","red");
+$("#phone_block").css({"padding-right":"0px"});
+$("#p_number").css({"text-decoration":"underline"},{"width":"300px"});
+$("#p_number").show('slow');
 
-
-
-
+// add below just to override anchors for now
+$("a").click(function(){
+    alert('These links are not funtion yet');
 
 });
+
+
+
+
+
+
+
+
+}); // end ready load function
